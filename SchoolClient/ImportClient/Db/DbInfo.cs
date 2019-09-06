2a2585d547437590b93537076ce40e986fd268fc
Pro Dev <prodev9999@gmail.com>
2019-09-07 05:59:08 +0800

    Created DBInfo class

---
+++ SchoolClient/ImportClient/Db/DbInfo.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportClient.Db
+{
+    public static class DbInfo
+    {
+        public static string server { get; set; }
+        public static int port { get; set; }
+        public static string uid { get; set; }
+        public static string password { get; set; }
+        public static string database { get; set; }
+
+        public static MySqlConnection connection { get; set; }
+    }
+}
