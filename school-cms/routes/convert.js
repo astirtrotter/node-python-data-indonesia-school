35837c6e82e73c25132b97619c6b455543b61c9e
Pro Dev <prodev9999@gmail.com>
2019-09-18 01:40:25 +0800

    Updated apis

--- school-cms/routes/convert.js
+++ school-cms/routes/convert.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', async function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('convert', { title: 'Convert' });
 });
 
